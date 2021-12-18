ff09f0e13e429acfc23ecc8fcbb1a445b358d742
Julian Stephens <julianstephens55@gmail.com>
2021-12-18 18:45:11 -0500

    Update config

--- lua/core/null-ls.lua
+++ lua/core/null-ls.lua
@@ -2,34 +2,36 @@ local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local null_ls_settings = {
-    sources = {
-        b.formatting.black,
-        b.formatting.codespell,
-        b.formatting.eslint_d,
-        b.formatting.stylelint,
-        b.formatting.stylua,
-        b.diagnostics.codespell,
-        b.formatting.prettier.with({
-            filetypes = {
-                "typescriptreact",
-                "typescript",
-                "javascriptreact",
-                "javascript",
-                "svelte",
-                "json",
-                "jsonc",
-                "css",
-                "html",
-                "scss",
-            },
-        }),
-        b.code_actions.gitsigns,
-        b.completion.spell
-    },
-    on_attach = function(client, bufnr)
-        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-    end
+	sources = {
+		b.formatting.black,
+		b.formatting.codespell,
+		b.formatting.eslint_d,
+		b.formatting.stylelint,
+		b.formatting.stylua,
+		b.diagnostics.codespell,
+		b.formatting.prettier.with({
+			filetypes = {
+				"typescriptreact",
+				"typescript",
+				"javascriptreact",
+				"javascript",
+				"svelte",
+				"json",
+				"jsonc",
+				"css",
+				"html",
+				"scss",
+				"markdown",
+			},
+		}),
+		b.code_actions.gitsigns,
+		b.completion.spell,
+	},
+	on_attach = function(client, bufnr)
+		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+	end,
 }
 
 -- Null LS
 null_ls.setup(null_ls_settings)
+
