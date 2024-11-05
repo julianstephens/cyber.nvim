e3063a25d49016c31b63d8e7c5d9eb1071d4efdf
Julian Stephens <julian@julianstephens.net>
2024-11-05 18:50:27 -0500

    fix: update to trouble v3

--- lua/core/trouble.lua
+++ lua/core/trouble.lua
@@ -1,7 +1,28 @@
-require('trouble').setup({
-    mode = "document_diagnostics",
-    auto_close = true,
-    action_keys = {
-      close = '<C-q>'
-    }
+require("trouble").setup({
+	modes = {
+		preview_diagnostics = {
+			mode = "diagnostics",
+			preview = {
+				type = "split",
+				relative = "win",
+				position = "right",
+				size = 0.3,
+			},
+			filter = { buf = 0 },
+		},
+		workspace_diagnostics = {
+			mode = "diagnostics",
+			filter = {
+				any = {
+					function(item)
+						return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+					end,
+				},
+			},
+		},
+	},
+	auto_close = true,
+	action_keys = {
+		close = "<C-q>",
+	},
 })
