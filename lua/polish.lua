00d0bc297efa7217291fa76037c5eedab38552fc
Julian Stephens <julian@julianstephens.net>
2026-07-02 15:56:34 -0400

    feat: update cursor style

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,6 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Enforces a vertical bar cursor with blinking in Insert (i) and Command-line Insert (ci) modes
+vim.opt.guicursor = "i-ci:ver25-blinkwait175-blinkon175-blinkoff150-Cursor/lCursor"
